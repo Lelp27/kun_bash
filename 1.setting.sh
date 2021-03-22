42ebcaf8623c02f0291a6967d671373ae94ec458
kun <tjdrns27@kribb.re.kr>
2021-03-22 20:19:13 +0900

    lab

--- 1.setting.sh
+++ 1.setting.sh
@@ -2,10 +2,14 @@
 
 reference=/home/kun/Row_data/DB/LacI_GFP/210315_LacI_GFP.fasta
 DB=${reference/.fasta/.mmi}
-echo $DB
-q_folder=/home/kun/210315/kun/20210315_1545_MC-110372_0_aga495_511d1d2d/fastq_pass/barcode09/filter
-echo $q_folder
+
+echo '############################################'
+
+echo 'DB = '$DB
+q_folder=/home/kun/210317/kun/20210317_1250_MC-110372_0_aga502_8de00847/filter/unclassified
+echo 'folder = '$q_folder
 files=$(ls $q_folder/*.fastq)
-echo $files
 outname=$(basename -s .mmi $DB)
-echo $outname
+echo 'outname : '$outname
+
+echo '############################################'
